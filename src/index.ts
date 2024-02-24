7f3a26989f3ecef7e1713d8d50d254c454cffaf6
Cosimo Matteini <dev.matteini@gmail.com>
2024-02-24 19:01:13 +0100

    create cli app

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,8 @@ import { main as compress } from "./images-compress"
 import { main as writeJson } from "./images-json"
 import * as path from "path"
 import * as Effect from "effect/Effect"
-import * as F from "effect/Function"
+import { Command } from "@effect/cli"
+import { NodeContext, NodeRuntime } from "@effect/platform-node"
 
 export const imageTypesRegex = /\.(png|jpeg|jpg|webp)$/i
 const sourceDirRelative = "./public/team-photos"
@@ -13,14 +14,32 @@ const processedDirAbsolute = path.join(sourceDirAbsolute, compressOutputDir)
 const finalImageSrcBaseUrl = `/team-photos/${compressOutputDir}`
 const jsonOutputFile = "images.json"
 
-await F.pipe(
-    Effect.promise(() => compress(sourceDirAbsolute, compressOutputDir)),
-    Effect.runPromise,
+const resizeImages = Command.make("resize-images", {}, () =>
+    Effect.gen(function* (_) {
+        yield* _(
+            Effect.promise(() =>
+                compress(sourceDirAbsolute, compressOutputDir),
+            ),
+        )
+
+        yield* _(
+            Effect.promise(() =>
+                writeJson(
+                    processedDirAbsolute,
+                    jsonOutputFile,
+                    finalImageSrcBaseUrl,
+                ),
+            ),
+        )
+    }),
 )
 
-await F.pipe(
-    Effect.promise(() =>
-        writeJson(processedDirAbsolute, jsonOutputFile, finalImageSrcBaseUrl),
-    ),
-    Effect.runPromise,
+const cli = Command.run(resizeImages, {
+    name: "Resize images",
+    version: "v1.0.0",
+})
+
+Effect.suspend(() => cli(process.argv)).pipe(
+    Effect.provide(NodeContext.layer),
+    NodeRuntime.runMain,
 )
